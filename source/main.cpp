10c124cf817d080aa455a19cf0e5c39e44a4c07a
Wade Gushikuma <yochillax@gmail.com>
2015-03-04 12:39:23 -0800

    grid draws properly

--- source/main.cpp
+++ source/main.cpp
@@ -16,26 +16,28 @@ int main( int argc, char* argv[] )
 	SetBackgroundColour(SColour(0x00, 0x00, 0x00, 0xFF));
 	
 	float sWidth, sHeight;
-	const int gWidth = 4;
+	const int gWidth = 20;
 
 	Graph myGraph(gWidth, gWidth);	//	create graph
 	std::vector<GraphNode*>::iterator beg = myGraph.m_aNodes.begin();
 	beg++;
 	std::vector<GraphNode*>::iterator end = myGraph.m_aNodes.end();
 	end--;
-	cout << myGraph.SearchDFSrec((*beg), (*end)) << endl;
+	
 
-	GraphNode display;
+	/*GraphNode display;
 	display = myGraph.FindNode(1, 1);
-	cout << display.m_iNodeNumber;
+	cout << display.m_iNodeNumber;*/
 
 	unsigned int spriteList[gWidth*gWidth];
+	sWidth = wWidth / gWidth;
+	sHeight = wHeight / gWidth;
 	for (int i = 0; i < gWidth; i++)
 	{
 		for (int j = 0; j < gWidth; j++)
 		{
-			spriteList[gWidth * i + j] = CreateSprite("./square.png", wWidth / gWidth, wHeight / gWidth, true);
-			MoveSprite(spriteList[gWidth * i + j], (wWidth / gWidth)*j + (wWidth / gWidth /2), (wHeight / gWidth)*i + (wHeight / gWidth/2));
+			spriteList[gWidth * i + j] = CreateSprite("./square.png", sWidth, sHeight, true);
+			MoveSprite(spriteList[gWidth * i + j], (sWidth)*j + (sWidth /2), (sHeight)*i + (sHeight/2));
 		}
 	}
 
@@ -44,10 +46,14 @@ int main( int argc, char* argv[] )
 	{
 		ClearScreen();
 
-		//DrawSprite(hello);
+		cout << myGraph.SearchDFS((*beg), (*end)) << endl;	//	return true if beg connect to end
 
-		for (int i = 0; i < 4; i++)
+		for (int i = 0; i < gWidth*gWidth; i++)+
 		{
+			if ((*beg)->m_bVisited)
+			{
+				spriteList[beg-myGraph.m_aNodes.begin()] = CreateSprite("./traversed.png", sWidth, sHeight, true);	//	this is fucked up: moves when sprite switch
+			}
 			DrawSprite(spriteList[i]);
 		}
 
